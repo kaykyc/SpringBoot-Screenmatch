1a13cdbe4c541e4f218d1f0db3da3e7441877320
kaykyc <akc.silva@discente.ufma.br>
2024-03-20 09:30:19 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/screenmatch/service/ScreenmatchApplication.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoApi();
+        var json = consumoApi.obterDados("http://www.omdbapi.com/?apikey=4b2df4f0&t=gilmore%20girls");
+        System.out.println(json);
+        ConverteDados conversor = new ConverteDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+    }
+}
