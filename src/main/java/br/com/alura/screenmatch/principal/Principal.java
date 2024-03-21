60377af8429676716692f54298cdc2d7b503ea06
kaykyc <akc.silva@discente.ufma.br>
2024-03-21 11:52:53 -0300

    Modelando

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
@@ -31,5 +32,11 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
 
+        for (int i=0; i< dados.totalTemporadas();i++){
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j= 0; j< episodiosTemporada.size();j++){
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
     }
 }
