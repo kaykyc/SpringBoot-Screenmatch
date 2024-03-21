1cbe0cf0ec39751b1e61875fe1164e1d215a715a
kaykyc <akc.silva@discente.ufma.br>
2024-03-21 11:01:07 -0300

    Modelando

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,35 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+    private final String ENDERECO="https://www.omdbapi.com/?apikey=4b2df4f0&t=";
+    public void exibeMenu(){
+        System.out.println("Digite o nome de sua série a ser pesquisada: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados( ENDERECO + nomeSerie.replace(" ", "+"));
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i=1; i<=dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+")+ "&season="+ i);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+    }
+}
--- src/main/java/br/com/alura/screenmatch/service/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/service/ScreenmatchApplication.java
@@ -1,6 +1,6 @@
 package br.com.alura.screenmatch.service;
 
-import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,11 +14,8 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("http://www.omdbapi.com/?apikey=4b2df4f0&t=gilmore%20girls");
-        System.out.println(json);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
+        Principal principal = new Principal();
+        principal.exibeMenu();
+
     }
 }
